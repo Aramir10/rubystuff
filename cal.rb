4bdd97aa65dcff729a3f329783162c1d9c1fa30e
Victor Emmanuel <victorgdl89@gmail.com>
2019-05-14 13:53:19 -0500

    First ruby coomit

---
+++ cal.rb
@@ -0,0 +1,15 @@
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+puts 1+2
+puts 2*3
+puts 5-8
+puts 9/2
+
+puts "How Many hours in a year"
+puts 365 * 24
+puts "How Many Minuts in a decade"
+   puts (60) * (24) * (365) * (10)
+   puts "Age in sec"
+    puts ((60) * (24) * 60)* (365) * (29)
