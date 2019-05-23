de6f01da745b0cd78df593a4b2adfebb6a911981
Victor Emmanuel <victorgdl89@gmail.com>
2019-05-23 14:35:51 -0500

    more files to the pile

---
+++ madlib.rb
@@ -0,0 +1,11 @@
+
+puts "Enter a Color:"
+color =gets.chomp()
+puts "Enter a Plural noun:"
+plural_noun =gets.chomp()
+puts "Enter a Celebrity:"
+celebrity =gets.chomp()
+
+puts("Roses Are " + color)
+puts( plural_noun +  " are blue")
+puts("I love "+ celebrity)
---
+++ theinput.rb
@@ -0,0 +1,8 @@
+
+puts "Enter one  number "
+num1 = gets.chomp
+
+puts "Enter Another number"
+num2 = gets.chomp
+
+puts( num1.to_f + num2.to_f5 )
